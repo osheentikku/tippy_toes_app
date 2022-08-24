f7c248d19587947ed123fe25eb5d8468a3d62aa8
osheentikku <32721916+osheentikku@users.noreply.github.com>
2022-08-24 15:12:04 -0700

    login button leads to new blank screen

--- lib/Screens/Welcome/components/body.dart
+++ lib/Screens/Welcome/components/body.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:tippy_toes/Screens/Login/login_screen.dart';
 import 'package:tippy_toes/Screens/Welcome/components/background.dart';
 import 'package:tippy_toes/constants.dart';
 
@@ -10,47 +11,60 @@ class Body extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // total height and width of screen
-    Size size = MediaQuery.of(context).size;
+    Size size = MediaQuery
+        .of(context)
+        .size;
 
     return Background(
-        child: SingleChildScrollView(
-          child: Column(
+      child: SingleChildScrollView(
+      child: Column(
       mainAxisAlignment: MainAxisAlignment.center,
       children: <Widget>[
-          FittedBox(
-            fit: BoxFit.fitWidth,
-            child: Text(
-              "Welcome to",
-              style: GoogleFonts.patrickHand(
-                  textStyle:
-                      TextStyle(fontSize: 45, fontWeight: FontWeight.bold)),
-            ),
-          ),
 
-          //tippy toes logo
-          SvgPicture.asset(
-            "assets/icons/Tippy-Toes-Day-Care-Logo-(text).svg",
-            height: size.height * 0.45,
-          ),
-
-          //space
-          SizedBox(height: size.height * 0.02),
-
-          //login button
-          RoundedButton(
-            text: "LOGIN",
-            press: (){},
-          ),
-
-          //sign up button
-          RoundedButton(
-            text: "SIGN UP",
-            color: kPrimaryLightColor,
-            press: (){},
-          ),
-      ],
+        //welcome text
+        FittedBox(
+    fit: BoxFit.fitWidth,
+      child: Text(
+        "Welcome to",
+        style: GoogleFonts.patrickHand(
+            textStyle:
+            TextStyle(fontSize: 45, fontWeight: FontWeight.bold)),
+      ),
     ),
-        ));
+
+        //tippy toes logo
+        SvgPicture.asset(
+        "assets/icons/Tippy-Toes-Day-Care-Logo-(text).svg",
+        height: size.height * 0.45,
+        ),
+
+        //space
+        SizedBox(height: size.height * 0.02),
+
+        //login button
+        RoundedButton(
+          text: "LOGIN",
+          press: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) {
+                  return LoginScreen();
+                },
+              ),
+            );
+          }
+        ),
+
+    //sign up button
+    RoundedButton(
+    text: "SIGN UP",
+    color: kPrimaryLightColor,
+    press: (){},
+    ),
+    ],
+    ),
+    ));
+    }
   }
-}
 
